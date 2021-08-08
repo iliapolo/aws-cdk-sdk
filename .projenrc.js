399478362e26a9e7e6d8fa846ae2e46dcd00c904
iliapolo <eli.polonsky@gmail.com>
2021-08-08 13:17:57 +0300

    mid work

--- .projenrc.js
+++ .projenrc.js
@@ -22,6 +22,7 @@ const project = new JsiiProject({
     'aws-sdk',
     'codemaker',
     'json-schema',
+    'jsii-docgen',
     'json2jsii',
     'typescript-parser',
     'node-color-log',
@@ -35,68 +36,21 @@ const project = new JsiiProject({
 
 const codegen = project.addTask('codegen');
 codegen.exec('ts-node --skip-project codegen/main.ts');
-// codegen may change the task definitions so we need to resynth
-// immediately after.
-codegen.exec(project.projenCommand);
+
+const docgen = project.addTask('docgen');
+docgen.exec('ts-node --skip-project scripts/docgen.ts');
+
+project.compileTask.prependSpawn(codegen);
+project.compileTask.spawn(docgen);
 
 project.gitignore.exclude('.sdk');
-project.gitignore.exclude('package.overrides.json');
-project.gitignore.exclude('tsconfig.overrides.json');
-project.gitignore.include('vendor/jsii.tgz');
 
-// ideally we would just do project.buildTask.preSpawn(codegen)
-// however - projen preloads all task definitions when it starts up, meaning
-// that it doesn't see changes made in process.
-// TODO: we can change projen to lazy load tasks, should we?
-project.setScript('build', 'npx projen codegen && npx projen build');
-
-const incrementalCompile = project.addTask('compile.incremental');
-
-// compile all clients
-for (const client of fs.readdirSync(`${__dirname}/src/clients`)) {
-  compileClient(incrementalCompile, client);
-}
-
-// compile index
-const compileIndex = project.addTask(`${incrementalCompile.name}:index`);
-compileIndex.exec('ts-node --skip-project scripts/compile-index.ts');
-incrementalCompile.spawn(compileIndex);
-
-// create the final assembly
-const assemble = project.addTask(`${incrementalCompile.name}:assemble`);
-assemble.exec('ts-node --skip-project scripts/assemble.ts');
-incrementalCompile.spawn(assemble);
-
-// swap the default compile step with the incremental compile
-// project.buildTask.steps.filter(s => s.spawn === project.compileTask.name)[0].spawn = incrementalCompile.name;
+// lots of code - need some more memory...
 project.compileTask.env('NODE_OPTIONS', '--max_old_space_size=5000');
+project.tasks.tryFind('eslint').env('NODE_OPTIONS', '--max_old_space_size=5000');
 
 project.eslint.addIgnorePattern('codegen/**');
 project.eslint.addIgnorePattern('scripts/**');
 project.eslint.addRules({ 'max-len': ['error', { code: 2000 }] });
 
 project.synth();
-
-function compileClient(compileTask, client) {
-
-  const compile = project.addTask(`${compileTask.name}:${client}`);
-  const clientSrcPath = `src/clients/${client}`;
-  const clientLibPath = `lib/clients/${client}`;
-
-  const tsConfigOverridesPath = `${clientSrcPath}/tsconfig.overrides.json`;
-  const manifestOverridesPath = `${clientSrcPath}/package.overrides.json`;
-
-  // config overrides for specific client compilation
-  fs.writeFileSync(tsConfigOverridesPath, JSON.stringify({ include: [`${clientSrcPath}/*.ts`] }));
-  fs.writeFileSync(manifestOverridesPath, JSON.stringify({ main: `${clientLibPath}/index.js`, types: `${clientLibPath}/index.d.ts` }));
-
-  // instruct jsii to use the overrides we created
-  compile.env('JSII_TSCONFIG_OVERRIDES_PATH', tsConfigOverridesPath);
-  compile.env('JSII_MANIFEST_OVERRIDES_PATH', manifestOverridesPath);
-  compile.exec('npx projen compile');
-
-  // save the sub assembly for later consolidation
-  compile.exec(`mv .jsii ${clientSrcPath}/.jsii`);
-
-  compileTask.spawn(compile);
-}
---
+++ scripts/docgen.ts
@@ -0,0 +1,17 @@
+import * as fs from 'fs';
+import * as docgen from 'jsii-docgen';
+
+async function main() {
+
+  const clients: string[] = fs.readdirSync(`${__dirname}/../src/clients`);
+
+  const docs = await docgen.Documentation.forProject(`${__dirname}/../`);
+
+  for (const client of clients) {
+    const reference = docs.render({ submodule: client, readme: false });
+    fs.writeFileSync(`${__dirname}/../src/clients/${client}/API.md`, reference.render());
+  }
+
+}
+
+void main();
